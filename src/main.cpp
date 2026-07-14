9db31d7304fe53d844f4432087e371e1e84ab3e2
tanoshiz <tanoshiiz.studio@gmail.com>
2026-07-14 11:23:25 +0700

    My Thrid Commit. Test Raylib a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Try Second Commit\n";
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "window name");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+
+		DrawRectangle(50, 50, 100, 100, { 255, 0, 0, 127 });
+		DrawRectangle(75, 75, 100, 100, { 0, 255, 0 ,127 });
+
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
